efd2b2d8abe759b3f3c4a6b25b5b9d1baca3a103
lisahuele <lisahuele@gmail.com>
2021-07-13 17:40:17 -0400

    manager object
    created new object and used jest to test method(s)

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,8 +2,6 @@ const Intern = require("../lib/Intern");
 const Employee = require("../lib/Employee");
 jest.mock("../lib/Employee");
 
-//school, getSchool(), and getRole() overridden to return "Intern"
-
 test("retrieve intern's school", () => {
     const school = "University of Toronto";
     const newIntern = new Intern(Employee.name, Employee.id, Employee.email, school);
